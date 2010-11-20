81f8add15e9295af105055c1055ab53506700780
Arthur Ulfeldt <arthur@ulfeldt.com>
2010-11-20 12:47:37 -0800

    dont install clojure system wide
    It's not really necessary to install clojure because leiningen and maven take care of that better.

--- build-dev-vm.sh
+++ build-dev-vm.sh
@@ -2,19 +2,19 @@
 set +x
 
 if [ $# -ne 3 ]; then
-    echo "$0: name task password"
+    echo "$0: vm's-name username full-name password"
     exit 1
 fi
 
-NAME=$1
-TASK=$2
-PASSWORD=$3
-ME="Arthur Ulfeldt"
+NAME="$2"
+TASK="$1"
+PASSWORD="$4"
+ME="$3"
 
 time vmbuilder kvm ubuntu \
 -v \
 -d $TASK \
---suit=maverick \
+--suite=maverick \
 --mem=1024 \
 --cpus=2 \
 --rootsize=15000 \
@@ -23,6 +23,10 @@ time vmbuilder kvm ubuntu \
 --pass="$PASSWORD" \
 --hostname="$TASK" \
 --domain='dev.cryptovide.com' \
+--ip=192.168.122.120 \
+--mask=255.255.255.0 \
+--gw=192.168.122.1 \
+--dns=208.67.222.222 \
 --addpkg=emacs \
 --addpkg=openssh-server \
 --addpkg=bash-completion \
@@ -39,6 +43,6 @@ time vmbuilder kvm ubuntu \
 --addpkg=gitk \
 --addpkg=openssh-server \
 --libvirt=qemu:///system \
---firstlogin firstboot.sh \
+--firstlogin /home/arthur/vm-scripts/firstboot.sh \
 #--execscript setup-clojure
 
--- firstboot.sh
+++ firstboot.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 set +x
 sudo dpkg-reconfigure tzdata
-return 0
-
+echo time to set a real password (or prese enter to skip)
 #install clojure
 mkdir ~/opt
-cd ~/opt
-git clone git://github.com/clojure/clojure.git
-cd clojure
-ant
-mkdir ~/.clojure
-cp clojure.jar ~/.clojure
-
-#install clojure-contrib
-cd ~/opt 
-git clone git://github.com/clojure/clojure-contrib.git 
-cd clojure-contrib 
-mvn package 
-cp target/clojure-contrib*.jar ~/.clojure/clojure-contrib.jar
-
-#setup bash clj alias
-echo <<EOF >> ~/.bashrc
-export CLOJURE_EXT=~/.clojure
-export PATH=$PATH:~/opt/clojure-contrib/launchers/bash
-alias clj=clj-env-dir
-EOF
 
 #emacs setup
 
@@ -37,14 +16,14 @@ git clone git://github.com/technomancy/clojure-mode.git
 cd ~/opt
 mkdir leiningen
 cd leiningen
-wget http://github.com/technomancy/leiningen/raw/stable/bin/lein
+wget --no-check-certificate http://github.com/technomancy/leiningen/raw/stable/bin/lein
 
 echo 'export PATH=$PATH:~/opt/leiningen' >> ~/.bashrc
 chmod +x lein
 ./lein self-install
 
 mv -f ~/.emacs.d ~/.emacs.d.bak
-git clone http://github.com/eschulte/emacs-starter-kit.git .emacs.d
+git clone http://github.com/eschulte/emacs-starter-kit.git ~/.emacs.d
 cd ~/.emacs.d/
 git submodule init
 git submodule update
@@ -54,7 +33,7 @@ cd ../../
 git checkout -b my-emacs-changes
 #skip latex stuff
 
-echo <<EOF >> ~/.emacs/init.el
+cat <<EOF >> ~/.emacs.d/init.el
 ;; clojure-mode
 (add-to-list 'load-path "~/opt/clojure-mode")
 (require 'clojure-mode)
@@ -67,3 +46,17 @@ echo <<EOF >> ~/.emacs/init.el
 (require 'slime)
 (slime-setup)
 EOF
+
+echo setting up git 
+echo what is your Full Name
+read fullname
+git config --global user.name "$fullname"
+echo what is your email address
+read email
+git config --global user.email "$email"
+
+git add init.el
+git commit -m 'initial clojure emacs configuration'
+
+echo you may need to log out and back in to load the profile changes
+
